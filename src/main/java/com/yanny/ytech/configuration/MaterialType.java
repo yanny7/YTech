86c1aa273855855511f689c984ae9dd651c0e627
Yanny <lalis.jan@gmailcom>
2023-08-09 21:29:30 +0200

    Used all wood types for wooden objects

--- src/main/java/com/yanny/ytech/configuration/MaterialType.java
+++ src/main/java/com/yanny/ytech/configuration/MaterialType.java
@@ -4,6 +4,8 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.EnumSet;
+
 public enum MaterialType {
     COPPER("copper", "Copper", hex2rgb("#B87333"), Tiers.IRON),
     GOLD("gold", "Gold", hex2rgb("#FFDF00"), Tiers.GOLD),
@@ -12,9 +14,18 @@ public enum MaterialType {
     TIN("tin", "Tin", hex2rgb("#808080"), Tiers.IRON),
 
     ACACIA_WOOD("acacia", "Acacia", -1, "wooden", Tiers.WOOD),
+    BIRCH_WOOD("birch", "Birch", -1, "wooden", Tiers.WOOD),
+    CHERRY_WOOD("cherry", "Cherry", -1, "wooden", Tiers.WOOD),
+    DARK_OAK_WOOD("dark_oak", "Dark Oak", -1, "wooden", Tiers.WOOD),
+    JUNGLE_WOOD("jungle", "Jungle", -1, "wooden", Tiers.WOOD),
+    MANGROVE_WOOD("mangrove", "Mangrove", -1, "wooden", Tiers.WOOD),
     OAK_WOOD("oak", "Oak", -1, "wooden", Tiers.WOOD),
+    SPRUCE_WOOD("spruce", "Spruce", -1, "wooden", Tiers.WOOD),
+
     FLINT("flint", "Flint", hex2rgb("#666666"), Tiers.WOOD),
     ;
+
+    public static final EnumSet<MaterialType> ALL_WOOD = EnumSet.of(ACACIA_WOOD, BIRCH_WOOD, CHERRY_WOOD, DARK_OAK_WOOD, JUNGLE_WOOD, MANGROVE_WOOD, OAK_WOOD, SPRUCE_WOOD);
 
     @NotNull public final String key;
     @NotNull public final String name;
