391b39acadd2dcdeed18f81ab1056fb5f72d1504
Yanny <lalis.jan@gmailcom>
2024-06-13 21:22:50 +0200

    Added JEI support

---
+++ src/main/java/com/yanny/ytech/compatibility/JeiCompatibility.java
@@ -0,0 +1,72 @@
+package com.yanny.ytech.compatibility;
+
+import com.mojang.logging.LogUtils;
+import com.yanny.ytech.compatibility.jei.*;
+import com.yanny.ytech.configuration.Utils;
+import com.yanny.ytech.registration.YTechItems;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+@JeiPlugin
+public class JeiCompatibility implements IModPlugin {
+    protected static final Logger LOGGER = LogUtils.getLogger();
+
+    @Override
+    public void registerCategories(@NotNull IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new DryingRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new TanningRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new MillingRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new SmeltingRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new BlockHitRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new AlloyingRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new HammeringRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new PotteryRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(@NotNull IRecipeRegistration registration) {
+        if (Minecraft.getInstance().level != null) {
+            ClientLevel level = Minecraft.getInstance().level;
+
+            registration.addRecipes(DryingRecipeCategory.RECIPE_TYPE, DryingRecipeCategory.getRecipes(level.getRecipeManager()));
+            registration.addRecipes(TanningRecipeCategory.RECIPE_TYPE, TanningRecipeCategory.getRecipes(level.getRecipeManager()));
+            registration.addRecipes(MillingRecipeCategory.RECIPE_TYPE, MillingRecipeCategory.getRecipes(level.getRecipeManager()));
+            registration.addRecipes(SmeltingRecipeCategory.RECIPE_TYPE, SmeltingRecipeCategory.getRecipes(level.getRecipeManager()));
+            registration.addRecipes(BlockHitRecipeCategory.RECIPE_TYPE, BlockHitRecipeCategory.getRecipes(level.getRecipeManager()));
+            registration.addRecipes(AlloyingRecipeCategory.RECIPE_TYPE, AlloyingRecipeCategory.getRecipes(level.getRecipeManager()));
+            registration.addRecipes(HammeringRecipeCategory.RECIPE_TYPE, HammeringRecipeCategory.getRecipes(level.getRecipeManager()));
+            registration.addRecipes(PotteryRecipeCategory.RECIPE_TYPE, PotteryRecipeCategory.getRecipes(level.getRecipeManager()));
+
+            registration.addItemStackInfo(YTechItems.GRASS_FIBERS.get().getDefaultInstance(), Component.translatable("text.ytech.info.grass_fibers"));
+        } else {
+            LOGGER.warn("JEI integration was not loaded! Level is null!");
+        }
+    }
+
+    @Override
+    public void registerRecipeCatalysts(@NotNull IRecipeCatalystRegistration registration) {
+        DryingRecipeCategory.registerCatalyst(registration);
+        TanningRecipeCategory.registerCatalyst(registration);
+        MillingRecipeCategory.registerCatalyst(registration);
+        SmeltingRecipeCategory.registerCatalyst(registration);
+        BlockHitRecipeCategory.registerCatalyst(registration);
+        AlloyingRecipeCategory.registerCatalyst(registration);
+        HammeringRecipeCategory.registerCatalyst(registration);
+        PotteryRecipeCategory.registerCatalyst(registration);
+    }
+
+    @NotNull
+    @Override
+    public ResourceLocation getPluginUid() {
+        return Utils.modLoc("jei_plugin");
+    }
+}
---
+++ src/main/java/com/yanny/ytech/compatibility/jei/DryingRecipeCategory.java
@@ -0,0 +1,91 @@
+package com.yanny.ytech.compatibility.jei;
+
+import com.yanny.ytech.YTechMod;
+import com.yanny.ytech.configuration.MaterialType;
+import com.yanny.ytech.configuration.Utils;
+import com.yanny.ytech.configuration.recipe.DryingRecipe;
+import com.yanny.ytech.registration.YTechBlocks;
+import com.yanny.ytech.registration.YTechRecipeTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class DryingRecipeCategory implements IRecipeCategory<DryingRecipe> {
+    public static final RecipeType<DryingRecipe> RECIPE_TYPE = RecipeType.create(YTechMod.MOD_ID, "drying", DryingRecipe.class);
+
+    private final Font font = Minecraft.getInstance().font;
+    private final IDrawable background;
+    private final IDrawable icon;
+    private final Component localizedName;
+
+    public DryingRecipeCategory(IGuiHelper guiHelper) {
+        ResourceLocation location = Utils.modLoc("textures/gui/jei.png");
+        background = guiHelper.createDrawable(location, 0, 0, 82, 34);
+        icon = guiHelper.createDrawableItemStack(new ItemStack(YTechBlocks.DRYING_RACKS.of(MaterialType.OAK_WOOD).get()));
+        localizedName = Component.translatable("gui.ytech.category.drying");
+    }
+
+    @NotNull
+    @Override
+    public RecipeType<DryingRecipe> getRecipeType() {
+        return RECIPE_TYPE;
+    }
+
+    @NotNull
+    @Override
+    public Component getTitle() {
+        return localizedName;
+    }
+
+    @NotNull
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @NotNull
+    @Override
+    public IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, DryingRecipe recipe, @NotNull IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 1, 9).addIngredients(recipe.ingredient());
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 61,  9).addItemStack(recipe.result());
+    }
+
+    @Override
+    public void draw(@NotNull DryingRecipe recipe, @NotNull IRecipeSlotsView recipeSlotsView, @NotNull GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        String text = (recipe.dryingTime() / 20) + "s";
+        int stringWidth = font.width(text);
+
+        //TODO render tooltip for best/worst biomes
+        guiGraphics.drawString(font, text, (getWidth() - stringWidth) / 2 - 4, 26, 0xFF808080, false);
+    }
+
+    public static List<DryingRecipe> getRecipes(@NotNull RecipeManager recipeManager) {
+        return recipeManager.getAllRecipesFor(YTechRecipeTypes.DRYING.get()).stream().map(RecipeHolder::value).toList();
+    }
+
+    public static void registerCatalyst(@NotNull IRecipeCatalystRegistration registration) {
+        YTechBlocks.DRYING_RACKS.blocks().forEach((block) -> registration.addRecipeCatalyst(new ItemStack(block.get()), RECIPE_TYPE));
+    }
+}
